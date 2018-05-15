c940235e6d5af10752742d2a60386b332dcdb474
Christian <christian@fivesqrd.com>
2018-05-15 12:05:51 +0200

    Fixed config path

--- src/Laravel/MutexServiceProvider.php
+++ src/Laravel/MutexServiceProvider.php
@@ -20,7 +20,7 @@ class MutexServiceProvider extends Support\ServiceProvider
      */
     public function boot()
     {
-        $source = realpath($raw = __DIR__ . '/../config/mutex.php') ?: $raw;
+        $source = realpath($raw = __DIR__ . '/../../config/mutex.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('mutex.php')]);
