e16f35aa7763b8b59ae94ae9ef93237d9af62ba2
Christian <christianjburger@gmail.com>
2025-06-17 14:21:29 +0200

    Use nanosecond sleep to increase randomisation

--- src/Mutex/Lock.php
+++ src/Mutex/Lock.php
@@ -22,7 +22,7 @@ class Lock
     {
         try {
             /* wait for a random number of seconds */
-            sleep(rand(0, 5));
+            time_nanosleep(mt_rand(0, 10), mt_rand(0, 750000000));
 
             if ($this->set($time)) {
                 return $this->getKey();
